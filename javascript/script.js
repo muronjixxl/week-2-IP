66fb20aa454e180647f0400812b5086552cb3b13
muronjixxl <muronjiii@gmail.com>
2021-07-19 11:50:53 +0300

    changes to date validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,56 +1,51 @@
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let fnames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    let mnames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    let gender = document.getElementById("gender").value;
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let fnames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let mnames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let gender = document.getElementById("gender").value;
 
 function validation() {
-    let date = document.getElementById("date").value;
-    let month = document.getElementById("month").value;
-    let year = document.getElementById("year").value;
-    if (date == "", month == "", year == "") {
-        console.log()
+    let date = parseInt(document.getElementById("date").value);
+    let month = parseInt(document.getElementById("month").value);
+    let year = parseInt(document.getElementById("year").value);
+    if (date == "" || month == "" || year == "") {
         alert("Invalid date")
-    } else{getday();}
+        return false;
+    } //else { getname(); }
 }
 
 
 let Akanname;
-
-if(date > 31 || date <= 0 || month == 2 && date > 29){
-    alert("Enter valid date");
-}else if(month > 12){
-    alert("Enter valid month");
-}else if(year < 1900 || year > 2021){
-    alert("Enter valid year")
-}
-
-
-
-
-var getday=function() {
-
-
-
-    let CC = parseInt(year.slice(0, 2));
-    let YY = parseInt(year.slice(2, 4));
-    let MM = parseInt(month);
-    let DD = parseInt(date);
-
-    let Dayoftheweek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
-    let day = Math.round(Dayoftheweek);
-
-    //let Dayoftheweek = days[parseInt(Dayoftheweek)];
-    //let fnames = fnames[parseInt(Dayoftheweek)];
-    //let mnames = mnames[parseInt(Dayoftheweek)];
-
-    if (gender === "Male") {
-        Akanname = mnames[day]
-        alert("your name is " +Akanname);
+function getname(){
+    if (year < 1900 && year > 2021) {
+        alert("Enter valid year")
     }
-    else  {
-        Akanname = fnames[day]
-        alert("Your name is "+Akanname);
+    else if (date <= 0 && date > 31) {
+        alert("Enter valid year")
+    }
+    else if (month > 12) {
+        alert("Enter valid month")
+    }
+
+
+
+
+    let getday = function () {
+
+        let CC = parseInt(year.slice(0, 2));
+        let YY = parseInt(year.slice(2, 4));
+        let MM = parseInt(month);
+        let DD = parseInt(date);
+
+        let Dayoftheweek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+        let day = Math.round(Dayoftheweek);
+
+        if (gender === "Male") {
+            Akanname = mnames[day]
+            alert("your name is " + Akanname);
+        }
+        else {
+            Akanname = fnames[day]
+            alert("Your name is " + Akanname);
+        }
     }
 }
-//console.log(Akanname)
-//document.getElementById("para").innerHTML = "You were born on" + Dayoftheweek + "your Akan name is" + Akanname + "."
