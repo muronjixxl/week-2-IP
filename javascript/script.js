3f6574674542a4a8c9f8322b2bcb7e2d4337b157
muronjixxl <muronjiii@gmail.com>
2021-08-04 12:27:10 +0300

    debugged js

--- javascript/script.js
+++ javascript/script.js
@@ -1,51 +1,70 @@
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let fnames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let mnames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-let gender = document.getElementById("gender").value;
+const akanmale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-function validation() {
-    let date = parseInt(document.getElementById("date").value);
-    let month = parseInt(document.getElementById("month").value);
-    let year = parseInt(document.getElementById("year").value);
-    if (date == "" || month == "" || year == "") {
-        alert("Invalid date")
-        return false;
-    } //else { getname(); }
+const akanfemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function getDate() {
+
+    let dateOutput = document.getElementById("DOB").value;
+    console.log(dateOutput);
+
+    let male = document.getElementById("male");
+
+    let female = document.getElementById("female");
+
+
+    if (dateOutput == "") {
+        alert("Please enter date.")
+
+    } else if (male.checked == false && female.checked == false) {
+        alert("Please enter your gender.")
+
+    }
+
+    date = new Date(dateOutput);
+    console.log(date);
+
+
+
+    CC = date.getFullYear();
+    CC = CC.toString().substr(0, 2);
+    console.log(CC);
+
+
+    YY = date.getFullYear();
+    YY = YY.toString().substr(2, 4);
+    console.log(YY);
+
+
+    MM = date.getMonth();
+    console.log(MM);
+
+
+    DD = date.getDate();
+    console.log(DD);
+
+
+    y = date.getDay();
+    let dd = Math.round(
+        (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+    );
+    console.log(y);
+
+    return y;
+}
+function validateForm() {
+    getDate();
+    if (male.checked) {
+        console.log(akanmale[y]);
+        document.getElementById("DISPLAY").innerHTML = "Your Akan Name is " + "<span>" + akanmale[y] + "</span>" + "<br>" + "meaning you were born on a " + date.toLocaleString('en-us', { weekday: 'long' });
+    }
+    else if (female.checked) {
+        console.log(akanfemale[y]);
+        document.getElementById("DISPLAY").innerHTML = "Your Akan Name is " + "<span>" + akanfemale[y] + "</span>" + "<br>" + "meaning you were born on a " + date.toLocaleString('en-us', { weekday: 'long' });
+    }
 }
 
 
-let Akanname;
-function getname(){
-    if (year < 1900 && year > 2021) {
-        alert("Enter valid year")
-    }
-    else if (date <= 0 && date > 31) {
-        alert("Enter valid year")
-    }
-    else if (month > 12) {
-        alert("Enter valid month")
-    }
+function onSubmit() {
+    validateForm();
 
-
-
-
-    let getday = function () {
-
-        let CC = parseInt(year.slice(0, 2));
-        let YY = parseInt(year.slice(2, 4));
-        let MM = parseInt(month);
-        let DD = parseInt(date);
-
-        let Dayoftheweek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-        let day = Math.round(Dayoftheweek);
-
-        if (gender === "Male") {
-            Akanname = mnames[day]
-            alert("your name is " + Akanname);
-        }
-        else {
-            Akanname = fnames[day]
-            alert("Your name is " + Akanname);
-        }
-    }
 }
